c7998d20754c8042b1736e551f823070a25a19ca
MashXP | マッシーXP <realsteelphong@gmail.com>
2025-04-09 13:30:50 +0700

    Jake the Dog!

--- src/main/java/skinexpansion_dtn/ModelLayerLocations.java
+++ src/main/java/skinexpansion_dtn/ModelLayerLocations.java
@@ -17,4 +17,5 @@ public class ModelLayerLocations {
     public static final ModelLayerLocation EEVEELUTION_SYLVEON = new ModelLayerLocation(DTNCommunitySkinExpansion.getRes("eeveelution_sylveon"), "main");
     public static final ModelLayerLocation MOONLESS = new ModelLayerLocation(DTNCommunitySkinExpansion.getRes("moonless"), "main");
     public static final ModelLayerLocation OSLO = new ModelLayerLocation(DTNCommunitySkinExpansion.getRes("oslo_nanatsu"), "main");
+    public static final ModelLayerLocation JAKE_ADVENTURE_TIME = new ModelLayerLocation(DTNCommunitySkinExpansion.getRes("jake_adventure_time"), "main");
 }
